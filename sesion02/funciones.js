1dafcc615fad10f37d340d3cf94b0be9efb6d6e1
Jymmy Dextre <jsdextre@hotmail.com>
2023-02-20 19:47:08 -0500

    Semana 02

---
+++ sesion02/funciones.js
@@ -0,0 +1,21 @@
+saludar();
+saludarParametro("Autor Jymmy Dextre");
+let resultado = sumarValores(5,6);
+saludarParametro(resultado);
+
+//funcion sin parametro
+function saludar() {
+console.log("Sesión 02");
+}
+
+//Funcion con parametro
+function saludarParametro(msg) {
+console.log(msg);
+
+}
+
+//Sumar dos valores
+function sumarValores(n1,n2) {
+return (n1+n2);
+}
+

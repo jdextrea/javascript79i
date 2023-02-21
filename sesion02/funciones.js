332e76d5ea41a4bbf19e686da26ab7fe8cd32bf3
Jymmy Dextre <jsdextre@hotmail.com>
2023-02-20 22:31:41 -0500

    Semana 02

--- sesion02/funciones.js
+++ sesion02/funciones.js
@@ -1,7 +1,8 @@
-saludar();
+/*saludar();
 saludarParametro("Autor Jymmy Dextre");
 let resultado = sumarValores(5,6);
 saludarParametro(resultado);
+*/
 
 //funcion sin parametro
 function saludar() {
@@ -19,3 +20,29 @@ function sumarValores(n1,n2) {
 return (n1+n2);
 }
 
+function elegirColor() {
+    //calcular el indice
+    let indice = document.getElementById('color').options.selectedIndex;
+    //Calcular el valor de la lista seleccionada.
+    let valor = document.getElementById('color').options[indice].text;
+    //Obtener el valor de cada lista
+    let color = document.getElementById('color').options[indice].value;
+    
+    switch(indice) {
+
+        case 1:
+            document.bgColor= color;
+        break;
+        case 2:
+            document.bgColor=color;
+        break;
+        case 3:
+            document.bgColor=color;
+        break;
+        case 0:
+            alert("seleccionar opcion correcta");
+        break;
+    }
+
+}
+document.getElementById('color').addEventListener("change",elegirColor);

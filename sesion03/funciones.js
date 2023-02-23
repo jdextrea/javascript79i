6e6e92d19d19af6828b072820d81c7f8f4b083df
Jymmy Dextre <jsdextre@hotmail.com>
2023-02-22 20:21:38 -0500

    Semana 03

--- sesion03/funciones.js
+++ sesion03/funciones.js
@@ -1,20 +1,23 @@
 function validar() {
-    //Validar si un campo tiene datos
+    //1ra Validar si un campo tiene datos
     let correo = document.getElementById('correo').value;
     if (correo==null || correo.length==0 ) {
     alert('Faltan datos');
         return false;
-    } else {
+    } 
 
-        alert('datos validados');
-    }
-
-    //Validar si un numero 
+    //2. Validar si un numero 
     let edad = document.getElementById('edad').value;
     if (isNaN(edad)) {
         alert('Ingresar un numero');
         return false;
     }
 
-    
+    //3. 
+    let perfil  = Number(document.getElementById('perfil').selectedIndex);
+    if (perfil==0) {
+        alert('Debe seleccionar un sexo');
+        return false;
+    } 
+
 }

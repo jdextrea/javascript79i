e2d6ad61c0a8261e40921455c303d8358ab706a1
Jymmy Dextre <jsdextre@hotmail.com>
2023-03-01 21:04:53 -0500

    Sesión 5

---
+++ Proyecto-Tienda-Web/admin/js/formulario-producto.js
@@ -0,0 +1,65 @@
+const formulario = document.getElementById('formulario-producto');
+const inputs = document.querySelectorAll('#formulario-producto input');
+const selects = document.querySelectorAll('formulario-producto select');
+
+const expresiones = {
+    inputModelo: /^[0-9]{1,6}$/,
+    inputProducto: /^.{4,40}$/,
+    inputDetalle: /^.{10,100}$/,
+    inputPeso: /^[0-9.]{1,6}$/,
+    inputMaterial: /^.{1,15}$/,
+    inputStock: /^[0-9]{1,6}$/
+}
+
+const campos = {
+    inputModelo: false,
+    inputProducto: false,
+    inputDetalle: false,
+    inputPeso: false,
+    inputMaterial: false,
+    inputStock: false
+
+}
+//Funciones Lambda
+const validarCampo = (expresion, input, campo) => {
+    if (expresion.test(input)) {
+        document.getElementById(´${campo}´).classList.add('is-valid');
+        document.getElementById(´${campo}´).classList.remove('is-valid');
+        campos[campo] = true;
+    } else {
+        document.getElementById(´${campo}´).classList.add('is-invalid');
+        document.getElementById(´${campo}´).classList.remove('is-invalid');
+        campos[campo] = true;
+    }
+}
+
+   const validarFormulario = (e) => {
+        switch (e.target.name) {
+            case "inputProducto":
+                validarCampo(expresiones.inputProducto,e.target,'inputProducto');
+            break;
+            case "inputModelo":
+                validarCampo(expresiones.inputModelo,e.target,'inputModelo');
+            break;
+            case "inputDetalle":
+                validarCampo(expresiones.inputDetalle,e.target,'inputDetalle');
+            break;
+            case "inputPeso":
+                validarCampo(expresiones.inputPeso,e.target,'inputPeso');
+            break;
+            case "inputMaterial":
+                validarCampo(expresiones.inputMaterial,e.target,'inputMaterial');
+            break;
+            case "inputStock":
+                validarCampo(expresiones.inputStock,e.target,'inputStock');
+            break;
+        }
+   } 
+
+
+//Funcion para recorrer todos los input foreach
+inputs.forEach((input) => {
+    input.addEventListener('keyup',validarFormulario);
+    input.addEventListener('blur',validarFormulario);
+    
+});

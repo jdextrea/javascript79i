32b6a0be39f5713510f8a8bf0664c8db36c037eb
Jymmy Dextre <jsdextre@hotmail.com>
2023-03-08 20:51:15 -0500

    sesion7

--- sesion07/js/funciones.js
+++ sesion07/js/funciones.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     const $select = $("#miSelect");
+    
     const opcionCambiada = () => {
         console.log("cambio");
     }
@@ -7,9 +8,26 @@ $(document).ready(function() {
     $select.change(opcionCambiada);
 
     //Crear una funcion para saber la eleccion de cada elemento
+    const agregar = () => {
+        let valor = new Date().getTime();
+        $select.append($("<option>", {
+            value: valor,
+            text: valor
+        }));
+    };
+
+
 
     //Crear un metodo para limpiar el select
     const limpiar = () => {
         $select.empty();
-    }
+    };
+
+    //Programar los botones
+   // $(document).ready(function() {
+    //($arbol).evento(funcion)
+    $("#btnAgregar").click(agregar);
+    $("#btnLimpiar").click(limpiar);
+
 });
+

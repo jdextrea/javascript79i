3ebb6e0534db9998ae8324f2a4446ce3c15c13e2
Jymmy Dextre <jsdextre@hotmail.com>
2023-03-08 19:41:02 -0500

    sesion7

--- sesion07/js/arreglos.js
+++ sesion07/js/arreglos.js
@@ -1,4 +1,18 @@
 let personas = ["Jaime","Melissa","Santiago","Carlos"];
 let nroElementos = personas.length;
 
+//Metodo para contar elementos
 console.log(nroElementos);
+
+//Para acceder a un valor del arreglo 
+//console.log(personas[2]);
+
+//Mostrar todos los elementos
+for (let x=0;x<nroElementos;x++) {
+    console.log(personas[x]);
+
+}
+
+//Crear un programa que permita mostrar los paises en una sola fila
+//Peru Chile Brazil Ecuador
+let paises = ["Peru","Chile","Brazil","Ecuador"];

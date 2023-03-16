bbffa92a9628df53bc8253b824974dda6753435b
Jymmy Dextre <jsdextre@hotmail.com>
2023-03-15 20:04:51 -0500

    sesion8

--- sesion08/js/json.js
+++ sesion08/js/json.js
@@ -6,4 +6,11 @@ console.log(objPersona.codigo);
 console.log(objPersona.correo);
 console.log(objPersona.clave);
 
+//Agregar un arreglo de notas
+let personaNotas = '{"codigo":"c01","correo":"jdextre@certus.edu.pe","clave":"1234","notas":[12,15]}';
+//Parse convierte una cadena a un objeto de javascript
+const objPersonaNotas = JSON.parse(personaNotas);
+let n1 = objPersonaNotas.notas[0];
+console.log(n1);
+
 
